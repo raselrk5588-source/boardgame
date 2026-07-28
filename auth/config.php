7ffaa8b92f329ae339b5f75a04d51cbfea9d8c9d
raselrk5588-source <raselrk5588@gmail.com>
2026-07-28 16:37:38 +0600

    Update config file

--- auth/config.php
+++ auth/config.php
@@ -2,7 +2,7 @@
 // config.php
 // এখানে আপনার BDApps এর App ID এবং Password দিন
 return [
-    'applicationId' => 'APP_139093',
-    'password' => '21b9eb4d3aeb97d2b65d022d6156080e'
+    'applicationId' => 'APP_139339',
+    'password' => '70bd320e304e6df56ba9e046b6f32ad5'
 ];
 ?>
